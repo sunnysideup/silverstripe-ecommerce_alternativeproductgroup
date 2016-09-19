a0168fabefab263919e7e1e40e31f62e511d1b19
www-data <sunnysideupdevs@gmail.com>
2016-09-19 18:48:29 +1200

    fix ups

--- _config.php
+++ _config.php
@@ -11,4 +11,3 @@
 //copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
 //===================---------------- START ecommerce_alternativeproductgroup MODULE ----------------===================
 //===================---------------- END ecommerce_alternativeproductgroup MODULE ----------------===================
-
--- code/ProductGroupAlternative.php
+++ code/ProductGroupAlternative.php
@@ -4,30 +4,35 @@
  * @package ecommerce
  * @subpackage ecommerce_alternativeproductgroup
  **/
-class ProductGroupAlternative extends ProductGroup {
+class ProductGroupAlternative extends ProductGroup
+{
 
-	/**
-	 * Standard SS variable.
-	 */
-	public static $singular_name = "Product Category Page Alternative View";
-		function i18n_singular_name() { return _t("ProductGroup.PRODUCTGROUPALTERNATIVE", "Product Category Page Alternative View");}
+    /**
+     * Standard SS variable.
+     */
+    public static $singular_name = "Product Category Page Alternative View";
+    public function i18n_singular_name()
+    {
+        return _t("ProductGroup.PRODUCTGROUPALTERNATIVE", "Product Category Page Alternative View");
+    }
 
-	/**
-	 * Standard SS variable.
-	 */
-	public static $plural_name = "Product Category Pages Alternative View";
-		function i18n_plural_name() { return _t("ProductGroup.PRODUCTGROUPSALTERNATIVE", "Product Category Pages Alternative View");}
+    /**
+     * Standard SS variable.
+     */
+    public static $plural_name = "Product Category Pages Alternative View";
+    public function i18n_plural_name()
+    {
+        return _t("ProductGroup.PRODUCTGROUPSALTERNATIVE", "Product Category Pages Alternative View");
+    }
 
 
-	static $icon = "ecommerce_alternativeproductgroup/images/treeicons/ProductGroupAlternative";
-
-
+    public static $icon = "ecommerce_alternativeproductgroup/images/treeicons/ProductGroupAlternative";
 }
-class ProductGroupAlternative_Controller extends ProductGroup_Controller {
-
-	function init() {
-		parent::init();
-		Requirements::themedCSS("ProductGroupAlternative");
-	}
-
+class ProductGroupAlternative_Controller extends ProductGroup_Controller
+{
+    public function init()
+    {
+        parent::init();
+        Requirements::themedCSS("ProductGroupAlternative");
+    }
 }
